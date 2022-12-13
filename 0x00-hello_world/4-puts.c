b4aaa3c1b048bb4c7a6a2b4bcf72416971842df9
Prfsr-Algebra <117760810+Prfsr-Algebra@users.noreply.github.com>
2022-12-13 10:44:45 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
-  /*program to show the work of put function*/
+  /*main: entry point*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
