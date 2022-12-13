2a330150ecafe0c9984d1ebdb259781720fb9a5b
Prfsr-Algebra <117760810+Prfsr-Algebra@users.noreply.github.com>
2022-12-13 11:43:08 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *    *
+ *
+ */
 int main(void)
 {
-<<<<<<< HEAD
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-=======
-    write(2,"and that piece of art is useful\" -Dora Korper, 2015-10-19\n", 59);
->>>>>>> 5a4ea96da9f2d6ab1c0bf39dbb15cf26ca9a2699
-    return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
