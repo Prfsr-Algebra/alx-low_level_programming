8c17e87f3aeb1c315126b55f019e7cd105f4224b
Prfsr-Algebra <117760810+Prfsr-Algebra@users.noreply.github.com>
2022-12-13 10:48:21 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main(void)
-  /*main: entry point
+/**main: entry point
   *return: always zero success
   */
+int main(void)
+  
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
